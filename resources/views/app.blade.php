f4be1a615dec24b89a4767879d9c55188f1cb82f
nataly <nataly@localhost.localdomain>
2023-01-26 21:58:42 -0600

    edicion de appblade

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,18 +3,25 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
-        <!-- Scripts -->
-        <!--  @routes-->
-        @vite('resources/js/app.js')
-        @inertiaHead
+          <!-- Styles -->
+          <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+          @livewireStyles
+      
+          <!-- Scripts -->
+          <script src="{{ asset('js/app.js') }}" defer></script>
+      
     </head>
-    <body class="font-sans antialiased">
-        @inertia
+    <body>
+        <div class="font-sans text-gray-900 antialiased">
+            {{ $slot }}
+        </div>
+        @livewireScripts
     </body>
 </html>
